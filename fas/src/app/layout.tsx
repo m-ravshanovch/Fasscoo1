3835e8f9d260e423a10f27b4d4e54356cca44e2e
Abdug'ani <xolmatjonovabdugani@gmail.com>
2025-02-22 03:43:09 +0500

    hi

--- fas/src/app/layout.tsx
+++ fas/src/app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`antialiased w-screen h-screen m-0 p-0`}
       >
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
