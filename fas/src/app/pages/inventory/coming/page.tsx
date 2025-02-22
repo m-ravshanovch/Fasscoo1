2835b676702f4ff5ab6bebfaede00d13fccc26c2
m-ravshanovch <muhammadqodirtoshqoziyev022@gmail.com>
2025-02-23 00:17:26 +0500

    dawda

--- fas/src/app/pages/inventory/coming/page.tsx
+++ fas/src/app/pages/inventory/coming/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     }[]>([])
 
     useEffect(() => {
-        axios.get("http://192.168.0.105:5000/Import/")
+        axios.get("http://172.20.10.2:5000/Import/")
             .then((res) => {
                 setImportData(res.data)
                 console.log(res.data)
